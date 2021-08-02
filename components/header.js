9cceb005ca55c55327aefd03264e8e768c69eaee
Ethan Patterson <eryanpatterson@gmail.com>
2021-08-02 14:04:48 -0400

    Added logic to header component to choose page headings based on Router.

--- components/header.js
+++ components/header.js
@@ -1,11 +1,25 @@
 import Link from 'next/link';
 import styles from './header.module.css'
-
+import { useRouter } from 'next/router';
 
 const Header = () => {
+    
+    const router = useRouter()
+    const path = router.pathname;
+    let pageName = ''
+
+    const pages = ['Home' , 'About' , 'Blog'];
+
+    path === '/' ? pageName = pages[0] : 
+        (path === '/about' ? pageName = pages[1] : pageName = pages[2])
+    
+
     return (
         <div>
             <header className={styles.header}>
+                <h1 className={styles.heading}>
+                    {pageName}
+                </h1>
                 <nav className={[styles.right, styles.bottom].join(' ')}>
                     <Link href="/">
                         <a className={styles.link}>Home</a>

8caa1063e8f7835d18e4213b7fa4a16a8c33579b
Ethan Patterson <eryanpatterson@gmail.com>
2021-08-05 14:43:33 -0400

    Added About page content

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,58 @@
 import Header from "../components/header";
+import styles from "../styles/about.module.css"
 
 export default function About() {
     return (
         <div>
             <Header />
-
+            <div className={styles.container}>
+                <h1>Experience:</h1>
+                <div className={styles.main}>
+                    <p>
+                        For the last few years, I have worked primarily as a church
+                        musician, first as worship leader for a church plant in 
+                        Denver, Colorado, and now as music director for a rural Lutheran
+                        Church in the Dayton area. Working in a church requires a lot
+                        of communication, flexibility, and creative problem-solving,
+                        and I will carry that experience with me into my first professional
+                        role as a developer. My experience as a church musician has also
+                        given me an appreciation, as a user, for web-based tools and platforms
+                        like Planning Center and Mailchimp; as I grow and progress as 
+                        a developer, I am continually more curious what makes these 
+                        applications work.
+                        
+                    </p>
+                </div>
+                <h1>Education:</h1>
+                <div className={styles.main}>
+                    <p>
+                        My journey to development has been non-traditional. I hold a 
+                        Bachelor of Arts degree in Music from Maryville College, a small
+                        liberal-arts school in the Foothills of the Smoky Mountains of Tennessee.
+                        In 2020, I completed a Master of Theological Studies degree at Trinity Lutheran
+                        Seminary, a graduate school of Capital University in Columbus, Ohio. 
+                        As a developer, musician, and theologian, I reject the false dichotomy between
+                        creative thinking and analytical thinking. Teams that master the synergy of 
+                        creativity and analysis are ulimately the ones who thrive and drive change,
+                        both in the marketplace and in society.
+                    </p>
+                </div>
+                <h1>Technologies:</h1>
+                <div className={styles.main}>
+                    <h2>I have experience with...</h2>
+                    <ul>
+                        <li>React</li>
+                        <li>Next.js</li>
+                        <li>Node.js</li>
+                        <li>Passport.js</li>
+                        <li>Bootstrap</li>
+                        <li>Vercel</li>
+                        <li>Heroku</li>
+                        <li>MongoDB Atlas</li>
+                        <li>Git/Github</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }

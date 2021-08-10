9b31775ec7b974f00abafcd6fb3126e8ebb6826a
Ethan Patterson <eryanpatterson@gmail.com>
2021-08-10 16:31:15 -0400

    Fixes and added site title

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -1,9 +1,8 @@
-import Header from "../../components/header";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from 'next/head';
 import Date from "../../components/date"
-import Footer from "../../components/footer"
 import Layout from "../../components/layout";
+import styles from "../../styles/post.module.css"
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.post)
@@ -30,7 +29,7 @@ export default function Post({ postData }) {
                 <title>{postData.title}</title>
             </Head>
 
-            <article>
+            <article className={styles.container}>
                 <h1>{postData.title}</h1>
                 <div>
                     <Date dateString={postData.date} />

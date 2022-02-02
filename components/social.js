2ac2328990c06e275e3693491b65c5356aea672f
Ethan Patterson <eryanpatterson@gmail.com>
2022-02-01 21:52:19 -0500

    Remove unneeded social icons

--- components/social.js
+++ components/social.js
@@ -6,21 +6,13 @@ export default function Social() {
 
     return (
         <ul className={styles.social}>
-            <li>
-                <Link href="http://github.com/eryanpatterson">
-                    <a target="_blank"><FaGithub /></a>
-                </Link>
-            </li>
+            
             <li>
                 <Link href="http://www.linkedin.com/in/eryanpatterson/">
                     <a target="_blank"><FaLinkedin /></a>
                 </Link>
             </li>
-            <li>
-                <Link href="http://twitter.com/eryanpatterson">
-                    <a target="_blank"><FaTwitter /></a>
-                </Link>
-            </li>
+            
         </ul>
     )
 }
